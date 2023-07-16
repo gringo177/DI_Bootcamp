33dfb236868289859f34c0aca56170484036d88f
gringo177 <72018709+gringo177@users.noreply.github.com>
2023-07-16 14:16:17 +0300

    Add files via upload

---
+++ gogogopython/main.py
@@ -0,0 +1,52 @@
+#Exercise1
+print("Hello world Hello world Hello world Hello world")
+
+#Exercise2
+print((99^3)*8)
+
+#Exercise3
+#  5 < 3 False
+#  3 == 3 True
+#  3 == "3" True
+#  "3" > 3 False
+#  "Hello" == "hello" False
+
+#Exercise4
+computer_brand = "Legion"
+print("I have a + computer_brand + computer")
+
+#Exercise5
+my_name = "Mikhael"
+my_age = "28"
+my_shoe_size = "42"
+my_info = "my name is Mikhael, my age is 28 and my shoe size is 42"
+print( my_info)
+
+#Exercise6
+a = "2"
+b = "1"
+if a > b:
+    print("Hello world")
+
+#Exercise7
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
+
+#Exercise8
+my_name = "Mikhael"
+user_name = input("What is your name? ")
+if my_name == user_name:
+    print("You have the same name as me! Nice.")
+
+
+#Exercise9
+user_height = "145"
+user_height = input("what is your height in inches")
+if user_height > "145":
+    print("you are tall enough to ride.")
+else:
+        print("you need to grow some more to ride.")
