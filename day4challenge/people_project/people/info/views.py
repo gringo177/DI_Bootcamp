d7f77189fac1610243317cae56de8eac8e9d362e
gringo177 <happyhell127@gmail.com>
2023-08-14 14:03:52 +0300

    week5day4challenge

---
+++ day4challenge/people_project/people/info/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('person/', views.display_person, name='display_person'),
+]
+
+name = 'Bob Smith'
+age = 35
+country = 'USA'
+
+people = ['bob','martha', 'fabio', 'john']
+
+all_people = [
+  {
+    'id': 1,
+    'name': 'Bob Smith',
+    'age': 35,
+    'country': 'USA'
+  },
+  {
+    'id': 2,
+    'name': 'Martha Smith',
+    'age': 60,
+    'country': 'USA'
+  },
+  {
+    'id': 3,
+    'name': 'Fabio Alberto',
+    'age': 18,
+    'country': 'Italy'
+  },
+  {
+    'id': 4,
+    'name': 'Dietrich Stein',
+    'age': 85,
+    'country': 'Germany'
+  }
+]
