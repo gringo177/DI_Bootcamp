2a6c530389111dca8468f325c892172839a08d8a
gringo177 <72018709+gringo177@users.noreply.github.com>
2023-09-10 17:17:47 +0300

    Add files via upload

---
+++ day2/products.js
@@ -0,0 +1,20 @@
+// products.js
+const products = [
+    {
+        name: "Product 1",
+        price: 19.99,
+        category: "Electronics",
+    },
+    {
+        name: "Product 2",
+        price: 29.99,
+        category: "Clothing",
+    },
+    {
+        name: "Product 3",
+        price: 9.99,
+        category: "Books",
+    },
+];
+
+module.exports = products;
