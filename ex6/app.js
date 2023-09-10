3e99314536eca3bd822d387af456b59f56bf89fd
gringo177 <72018709+gringo177@users.noreply.github.com>
2023-09-10 17:39:35 +0300

    Add files via upload

---
+++ ex6/app.js
@@ -0,0 +1,10 @@
+
+const chalk = require('chalk');
+
+
+const colorfulText = chalk.bold.green('Hello, NPM Beginner!');
+const styledText = chalk.bgBlue.white(' This is a colorful message. ');
+
+
+console.log(colorfulText);
+console.log(styledText);
