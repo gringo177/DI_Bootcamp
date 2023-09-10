ed6d8b576822e19c2ebb9557a17ce040958cd557
gringo177 <72018709+gringo177@users.noreply.github.com>
2023-09-10 17:39:50 +0300

    Add files via upload

---
+++ ex7/file-explorer/read-directory.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs');
+
+
+const directoryPath = './';
+
+
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    console.log('Files in the directory:');
+    files.forEach((file, index) => {
+        console.log(`${index + 1}. ${file}`);
+    });
+});
