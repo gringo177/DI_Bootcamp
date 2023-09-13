392f969faa13a627b686d2daacff472217b63f65
gringo177 <72018709+gringo177@users.noreply.github.com>
2023-09-13 14:49:01 +0300

    Add files via upload

---
+++ day2challenge/challenge/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css'; 
+import CarouselComponent from './CarouselComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Carousel</h1>
+      <CarouselComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ day2challenge/challenge/src/CarouselComponent.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
+
+
+const CarouselComponent = () => {
+  return (
+    <div className="carousel-container">
+      <Carousel>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Macao" />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan" />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas" />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselComponent;
